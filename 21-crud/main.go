5632f1457513533d2b604e74a50cbbe66bfe230c
Marcos H Braz <marcosbrazz@gmail.com>
2024-06-20 16:21:42 -0300

    CRUD API
    - Get
    - Update
    
    Signed-off-by: Marcos H Braz <marcosbrazz@gmail.com>

--- 21-crud/main.go
+++ 21-crud/main.go
@@ -13,6 +13,9 @@ func main() {
 
 	router := mux.NewRouter()
 	router.HandleFunc("/user", services.InsertUser).Methods(http.MethodPost)
+	router.HandleFunc("/users", services.GetUsers).Methods(http.MethodGet)
+	router.HandleFunc("/user/{id}", services.GetUser).Methods(http.MethodGet)
+	router.HandleFunc("/user/{id}", services.UpdateUser).Methods(http.MethodPut)
 
 	fmt.Println("Listening on port 5001")
 	log.Fatal(http.ListenAndServe(":5001", router))
--- 21-crud/services/services.go
+++ 21-crud/services/services.go
@@ -2,10 +2,14 @@ package services
 
 import (
 	"crud/dao"
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 type User struct {
@@ -62,5 +66,116 @@ func InsertUser(w http.ResponseWriter, r *http.Request) {
 
 	w.Write([]byte(fmt.Sprintf("Created %d\n", id)))
 	w.WriteHeader(http.StatusOK)
+
+}
+
+func GetUsers(w http.ResponseWriter, r *http.Request) {
+	dao, error := dao.Getconnection()
+	if error != nil {
+		writeError(error, 500, "Error getting db connection", &w)
+		return
+	}
+
+	rows, error := dao.Query("SELECT * FROM USERS")
+	if error != nil {
+		writeError(error, 500, "Query failed", &w)
+		return
+	}
+	defer rows.Close()
+
+	var users []User
+	for rows.Next() {
+		var user User
+		if error := rows.Scan(&user.Name, &user.Age, &user.Id); error != nil {
+			writeError(error, 500, "Failed to scan results from database", &w)
+			return
+		}
+		users = append(users, user)
+	}
+
+	if error := json.NewEncoder(w).Encode(users); error != nil {
+		writeError(error, 500, "Failed to encode json", &w)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+}
+
+func GetUser(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	id, error := strconv.ParseUint(params["id"], 10, 32)
+	if error != nil {
+		writeError(error, http.StatusInternalServerError, "Failed to parse id", &w)
+		return
+	}
+
+	db, error := dao.Getconnection()
+	if error != nil {
+		writeError(error, http.StatusInternalServerError, "Failed to get db connection", &w)
+		return
+	}
+
+	defer db.Close()
+
+	var user User
+	row := db.QueryRow("select * from USERS where ID = ?", id)
+	if error := row.Scan(&user.Name, &user.Age, &user.Id); error != nil {
+		if error == sql.ErrNoRows {
+			writeError(error, http.StatusNotFound, "No rows", &w)
+		} else {
+			writeError(error, http.StatusInternalServerError, "Failed on query", &w)
+		}
+		return
+	}
+
+	if error := json.NewEncoder(w).Encode(user); error != nil {
+		writeError(error, http.StatusInternalServerError, "Failed on json encode", &w)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+
+}
+
+func UpdateUser(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	id, error := strconv.ParseUint(params["id"], 10, 32)
+	if error != nil {
+		writeError(error, http.StatusInternalServerError, "Failed to parse id", &w)
+		return
+	}
+
+	var user User
+	if error := json.NewDecoder(r.Body).Decode(&user); error != nil {
+		writeError(error, http.StatusInternalServerError, "Failed to decode body", &w)
+		return
+	}
+
+	db, error := dao.Getconnection()
+	if error != nil {
+		writeError(error, http.StatusInternalServerError, "Failed to connect to db", &w)
+		return
+	}
+
+	defer db.Close()
+
+	stmt, error := db.Prepare("update USERS set NAME = ?, AGE = ? where ID = ?")
+	if error != nil {
+		writeError(error, http.StatusInternalServerError, "Failed to prepare query statement", &w)
+		return
+	}
+	defer stmt.Close()
+
+	result, error := stmt.Exec(user.Name, user.Age, id)
+	if error != nil {
+		writeError(error, http.StatusInternalServerError, "Failed to on updating", &w)
+		return
+	}
+
+	affected, error := result.RowsAffected()
+	if error != nil {
+		writeError(error, http.StatusInternalServerError, "Failed result", &w)
+		return
+	}
+	w.WriteHeader(http.StatusNoContent)
+	fmt.Printf("Updated %d rows\n", affected)
 
 }
